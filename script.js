61e8954498b1784b36fbf440aa817b9256b46643
Qoey04 <wanguiqoey@gmail.com>
2025-08-11 12:24:36 +0300

    Initial commit of my portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: simple form submission message
+document.querySelector("form").addventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you! Your message has been sent.");
+});
